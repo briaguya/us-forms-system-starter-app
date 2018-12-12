768fbfda261049506cadae42ffce9499cad447c1
Brian Smith <briaguya@gmail.com>
2018-12-12 02:21:57 -0600

    basic start

---
+++ js/components/PlaceholderDisplayWidget.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import classnames from "classnames";
+
+export default function PlaceholderDisplayWidget(props) {
+  return <div>Placeholder</div>;
+}
--- js/config/form.js
+++ js/config/form.js
@@ -1,44 +1,56 @@
-import Introduction from '../components/Introduction.jsx';
+import Introduction from "../components/Introduction.jsx";
+import PlaceholderDisplayWidget from "../components/PlaceholderDisplayWidget.jsx";
 
 const formConfig = {
-  title: 'Form',
-  subTitle: 'Test',
-  formId: '',
-  urlPrefix: '/',
-  trackingPrefix: 'form-',
-  transformForSubmit: '',
-  submitUrl: '',
+  title: "Form",
+  subTitle: "Test",
+  formId: "",
+  urlPrefix: "/",
+  trackingPrefix: "form-",
+  transformForSubmit: "",
+  submitUrl: "",
   introduction: Introduction,
-  confirmation: '',
+  confirmation: "",
   defaultDefinitions: {},
   chapters: {
     firstChapter: {
-      title: 'First Chapter',
+      title: "First Chapter",
       pages: {
         firstPage: {
-          path: 'first-chapter/first-page',
-          title: 'First Page',
-          uiSchema: {},
+          path: "first-chapter/first-page",
+          title: "First Page",
           schema: {
-            type: 'object',
-            properties: {}
-          }
-        },
-        secondPage: {
-          path: 'first-chapter/second-page',
-          title: 'Second Page',
-          uiSchema: {},
-          schema: {
-            type: 'object',
-            properties: {}
+            type: "object",
+            properties: {
+              arrayOfThings: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    thing: {
+                      type: "string",
+                      enum: ["firstCommon", "secondCommon", "other"],
+                      enumNames: [
+                        "First common option",
+                        "Second common option",
+                        "Other"
+                      ]
+                    },
+                    otherThing: { type: "string" }
+                  }
+                }
+              }
+            }
+          },
+          uiSchema: {
+            arrayOfThings: {
+              "ui:options": {
+                viewField: PlaceholderDisplayWidget,
+                addable: true
+              }
+            }
           }
         }
-      }
-    },
-    secondChapter: {
-      title: 'Second Chapter',
-      pages: {
-
       }
     }
   }
